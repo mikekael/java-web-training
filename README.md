60badf6ec1103ea81a49ac00b15cdb5ea4920259
Mike <46599231+mikekael@users.noreply.github.com>
2022-05-03 09:22:04 +0000

    - updated readme and remove excess new line

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning Java Servlet
+
+This repository contains the necessary source code for learning Java Servlet and JSP using VScode
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
