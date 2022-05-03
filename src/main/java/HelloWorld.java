128c9cb9b123c752ce0467310e07d8d7655cad62
Mike <46599231+mikekael@users.noreply.github.com>
2022-05-03 09:20:59 +0000

    - added initial files with hello world servlet

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // set content type
+    response.setContentType("text/plain");
+
+    PrintWriter writer = response.getWriter();
+
+    writer.println("Hello World!");
+
+    writer.close();
+  }
+}
